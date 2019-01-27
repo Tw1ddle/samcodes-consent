06d60307fb4775d6ad7a21335a6517a2e804bc03
sam <samcodesit@gmail.com>
2019-01-27 01:35:43 +0000

    Remove unused lifecycle method overrides

--- dependencies/samcodes-consent/src/com/samcodes/consent/ConsentExtension.java
+++ dependencies/samcodes-consent/src/com/samcodes/consent/ConsentExtension.java
@@ -203,34 +203,4 @@ public class ConsentExtension extends Extension
 		}
 		return ConsentStatus.UNKNOWN;
 	}
-	
-	@Override
-	public void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
-	
-	@Override
-	public void onStart() {
-		super.onStart();
-	}
-	
-	@Override
-	public void onResume() {
-		super.onResume();
-	}
-	
-	@Override
-	public void onPause() {
-		super.onPause();
-	}
-	
-	@Override
-	public void onStop() {
-		super.onStop();
-	}
-	
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-	}
 }
